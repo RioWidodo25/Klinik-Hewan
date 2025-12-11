a0b3b78057ad65847c683aec87d914a573aa62e5
Riowidodo <riosport125@gmail.com>
2025-12-11 20:41:51 +0700

    Update Filament admin brand logo to use default /images/logo.png

--- resources/views/filament/admin/brand.blade.php
+++ resources/views/filament/admin/brand.blade.php
@@ -1,11 +1,4 @@
-@php
-    $footerSettings = \App\Models\FooterSetting::getSettings();
-    $logoUrl = $footerSettings->logo ? asset('storage/' . $footerSettings->logo) : null;
-@endphp
-
 <div class="flex items-center gap-3">
-    @if($logoUrl)
-        <img src="{{ $logoUrl }}" alt="Logo" class="h-10">
-    @endif
+    <img src="/images/logo.png" alt="Logo" class="h-10">
     <span class="text-xl font-bold text-gray-800 dark:text-white">A2 VET</span>
 </div>
